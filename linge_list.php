850271e90f7419a2c288b038b289b162b1440849
Abigail Mira <36951677+AbigailMira@users.noreply.github.com>
2020-01-16 16:45:16 +0100

    Update linge_list.php
    changements dans la base de données : création dela table appartenance

--- linge_list.php
+++ linge_list.php
@@ -15,12 +15,11 @@
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">Type</th>
-	  <th scope="col">Lit</th>
-      <th scope="col">Thème</th>
-	  <th scope="col">Rangé</th>
-	  <th scope="col">Linge assorti</th>
-	  
+        <th scope="col">Type</th>
+        <th scope="col">Thème/Couleur</th>
+        <th scope="col">Utilisation</th>
+        <th scope="col">Rangé</th>
+        <th scope="col">Linge assorti</th>	  
     </tr>
   </thead>
   <tbody>      
@@ -29,18 +28,8 @@
     foreach ($linge as $row){
     $piece_linge = getPieceForLinge($row["idlinge"]);
     echo "<tr>
-    <td>".$row['libelle_linge']."</td>     
-    <td>";
-    if ($row['largeur'] == 70){
-            echo "Lit d'enfant Ikea";
-        }
-        elseif ($row['largeur'] == 160){
-            if ($row['couleur'] == 'Pourpre' || $row['couleur'] == 'Safran') {
-                echo "Lit des parents";
-            }            
-        }
-    echo "</td>
-    <td>";
+    <td>".$row['libelle_linge']."</td>  
+        <td>";
     if ($row['theme'] != NULL){
         echo "Imprimé ".$row['theme'];
         }
@@ -48,6 +37,40 @@
         echo $row['couleur'];
     }
     echo "</td>
+    <td>";
+    switch ($row['largeur']) {
+        case 60:
+            echo "Oreiller carré";
+            break;
+        case 70:
+            echo "Lit d'enfant Ikea";
+            break;
+        case 80:
+            echo "Matelas pliant";
+            break;
+        case 90:
+            echo "Matelas 1 place";
+            break;
+        case 140:
+            echo "Couette 1p.";
+            break;        
+        case 160:
+            echo "Matelas 2 places";
+            break;
+        case 180:
+            echo "Lit 1 places";
+            break;
+        case 200:
+            echo "Couette des invités 2p.";
+            break;
+        case 220:
+            echo "Couette des parents";
+            break;
+        default:
+            echo "?";
+            break;
+    }
+    echo "</td>    
     <td>".$piece_linge['libelle_piece']."</td>
     </tr>";
     }?>
